71ca994425d0007c27494f7c08b7aca31f365332
Ryan Urabe <rurabe@gmail.com>
2018-05-01 22:21:10 -1000

    got score rendering

--- src/components/mortgages_card/mortgages_card.js
+++ src/components/mortgages_card/mortgages_card.js
@@ -10,12 +10,26 @@ import styles from './mortgages_card.sass'
 
 class MortgagesCard extends React.PureComponent {
   render(){
+    let rating;
+    if(this.props.mortgage.score){
+      rating = (
+        <div className="rating">
+          <span className="grade">{this.props.mortgage.grade}</span>
+          <span className="score">({Math.floor(100 *this.props.mortgage.score).toFixed()})</span>
+        </div>
+      );
+    }
+
     return (
       <li className="card border-primary mortgages-card">
         <div className="card-header">
           <div className="row">
             <div className="col-4">
-              <input className="form-control" placeholder="Lender" value={this.props.mortgage.lender} onChange={this.updateLender}/>
+              <input className="form-control" placeholder="Lender" value={this.props.mortgage.lender || ''} onChange={this.updateLender}/>
+            </div>
+            <div className="col-6"></div>
+            <div className="col-2">
+              {rating}
             </div>
           </div>
         </div>
